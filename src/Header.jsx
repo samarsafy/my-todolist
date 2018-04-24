4da72023b3ee3c9f06d1b83c60e1cce4d39c733e
Samar <samarsafy@yahoo.de>
2018-04-24 15:14:35 +0200

    add Header

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Header;
