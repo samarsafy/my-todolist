2bcc5499e245edd2a6917dd16636ecb5f901de4a
Fakher Alhamad <fakher1281992@gmail.com>
2018-04-25 13:12:12 +0200

    styling the form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'materialize-css/dist/js/materialize.min.js';
 import React from 'react';
 import './App.css';
 import CreatePost from './CreatePost';
-import Post from './Post';
 import PostList from './PostList';
 import Header from './Header';
 
@@ -11,18 +10,22 @@ class App extends React.Component {
   constructor(props){
   super(props);
   this.state = {
+    db : JSON.parse(localStorage.getItem('todolist')) || [],
+    posts: []
 
   };
   }
-  
+
+
   render() {
+
     return (
       <div className="container">
         <div className="row">
           <div className="col s12 m4 l8">
             <Header />
               <CreatePost />
-              <Post />
+              
             <PostList />
           </div>
         </div>
--- src/Post.jsx
+++ src/Post.jsx
@@ -10,9 +10,18 @@ class Post extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <p>Hallo from Post component</p>
-      </div>
+      <div>
+ 
+    <ul id="target" className="collection">
+      <li id="template" className="collection-item">
+      <label>
+        <input type="checkbox" />
+        <span>Red</span>
+      </label>
+      <a href="#!" className="secondary-content"><i className="material-icons">cancel</i></a>
+      </li>
+    </ul>  
+    </div>    
     );
   }
 }
