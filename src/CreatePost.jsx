6d4b73c6f2dd4aab965dde6aeec0f6d2b5182371
Fakher Alhamad <fakher1281992@gmail.com>
2018-04-25 11:03:48 +0200

    styling the form

--- src/CreatePost.jsx
+++ src/CreatePost.jsx
@@ -7,10 +7,10 @@ class CreatePost extends React.Component {
     return (
       
         <div className="App">
-          <form action="#" clasName=" form-inline mb-5">
+          <form action="#" className=" form-inline mb-5">
             <div className="form-group">
-              <label for="todo">Add Todo:</label>
-              <input type="text" className="text-white form-control bg-dark border-0 mr-2 w-100" value="milk" id="input" aria-describedby="emailHelp" name="input" placeholder="Buy milk"/>
+              <label htmlFor="todo">Add Todo:</label>
+              <input type="text" className="text-white form-control bg-dark border-0 mr-2 w-100" defaultValue="milk" id="input" aria-describedby="emailHelp" name="input" placeholder="Buy milk"/>
             </div>
             <button type="submit" className="btn btn-dark">Submit</button>
           </form>
--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,3 +1,5 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 import React from "react";
 
 
@@ -9,9 +11,25 @@ class PostList extends React.Component {
   }
 
   render() {
-    return <div className="App">
-        <p>Hallo from PostList component</p>
-      </div>;
+    return (
+    	<div>
+        <h1>Todos:</h1>
+        <small>
+        	<a className="text-success mr-3" href="?state=false">show open</a>
+        	<a className="text-success mr-3" href="?state=true">show done</a>
+        	<a className="text-success mr-3" href="./index.html">show all</a>
+        </small>
+    <ul className="collection">
+      <li className="collection-item">
+      <label>
+        <input type="checkbox" />
+        <span>Red</span>
+      </label>
+      <a href="#!" className="secondary-content"><i className="material-icons">cancel</i></a>
+      </li>
+    </ul>      
+      </div>
+    );
   }
 }
 
